96d81c67c38eab6c26f006017229672b047b1e2a
Marcin Suder <marcin.code.github@gmail.com>
2025-12-18 19:40:58 +0100

    add functionality to sum all items

--- Zadanie3.2.py
+++ Zadanie3.2.py
@@ -3,6 +3,7 @@ shopping_list = {
     "piekarnia": ['chleb', 'pączek', 'bułki'],
     "warzywniak": ['marchew', 'seler', 'rukola']
 }
-
+sum = 0
 for key, value in shopping_list.items():
     print(f"Idę do {key.capitalize()} i kupuję tam {[value.capitalize() for value in value]}")
+    sum += len(value)
