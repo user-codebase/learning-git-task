22fc200a8faf1673dd7a9a6e76a67c20509c5bfd
Marcin Suder <marcin.code.github@gmail.com>
2025-12-18 19:58:28 +0100

    add new shop - sklep_elektroniczny - with items

--- Zadanie3.2.py
+++ Zadanie3.2.py
@@ -2,7 +2,8 @@
 shopping_list = {
     "piekarnia": ['chleb', 'pączek', 'bułki'],
     "warzywniak": ['marchew', 'seler', 'rukola'],
-    "kwiacirania": ['róże', 'tulipany', 'stokrotki']
+    "kwiacirania": ['róże', 'tulipany', 'stokrotki'],
+    "sklep_elektroniczny": ['klawiatura', 'myszka']
 }
 sum = 0
 for key, value in shopping_list.items():
