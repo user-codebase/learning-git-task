dc54850c2769daa04ef69a2e166e5aef2265474b
Marcin Suder <marcin.code.github@gmail.com>
2025-12-18 19:57:25 +0100

    add new shop - kwiaciarnia - with items

--- Zadanie3.2.py
+++ Zadanie3.2.py
@@ -1,9 +1,12 @@
 # Zadanie 3.2
 shopping_list = {
     "piekarnia": ['chleb', 'pączek', 'bułki'],
-    "warzywniak": ['marchew', 'seler', 'rukola']
+    "warzywniak": ['marchew', 'seler', 'rukola'],
+    "kwiacirania": ['róże', 'tulipany', 'stokrotki']
 }
 sum = 0
 for key, value in shopping_list.items():
     print(f"Idę do {key.capitalize()} i kupuję tam {[value.capitalize() for value in value]}")
     sum += len(value)
+
+print(f"W sumie kupuję {sum} produktów.")
