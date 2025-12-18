07ca882c7d4d9faa491bb5dbd57479554d7f9e61
Marcin Suder <marcin.code.github@gmail.com>
2025-12-18 19:39:35 +0100

    add for loop to display names of shops and items

--- Zadanie3.2.py
+++ Zadanie3.2.py
@@ -3,3 +3,6 @@ shopping_list = {
     "piekarnia": ['chleb', 'pączek', 'bułki'],
     "warzywniak": ['marchew', 'seler', 'rukola']
 }
+
+for key, value in shopping_list.items():
+    print(f"Idę do {key.capitalize()} i kupuję tam {[value.capitalize() for value in value]}")
